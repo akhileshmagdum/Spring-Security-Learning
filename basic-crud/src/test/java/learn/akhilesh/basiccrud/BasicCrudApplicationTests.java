44b679aaf016a60d867c5ae78b23301ef48de060
akhileshmagdum <akhileshmagdum@gmail.com>
2023-02-17 21:44:21 +0530

    Basic crud application setup with h2 database

---
+++ basic-crud/src/test/java/learn/akhilesh/basiccrud/BasicCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.akhilesh.basiccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
