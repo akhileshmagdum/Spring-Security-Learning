8fd9eb5fcbe4700e99393078ce228163b5f731b6
akhileshmagdum <akhileshmagdum@gmail.com>
2023-02-26 11:32:49 +0530

    Basic in memory authentication with InMemoryUserDetailsManager

--- basic-crud/src/main/java/learn/akhilesh/basiccrud/config/SecurityConfig.java
+++ basic-crud/src/main/java/learn/akhilesh/basiccrud/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,14 +22,31 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests()
-                .requestMatchers("/home/**","/employee/all").permitAll()
+                .requestMatchers("/home/**").permitAll()
                 //here MVC matchers can also be used for path variables
-                .requestMatchers("/employee/{id}").authenticated()
+                .requestMatchers("/employee/all").hasRole("ADMIN")
+                .requestMatchers("/employee/{id}").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .and()
                 .httpBasic();
         return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin_user")
+                .roles("ADMIN")
+                .password("pass")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("simple_user")
+                .roles("USER")
+                .password("word")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
