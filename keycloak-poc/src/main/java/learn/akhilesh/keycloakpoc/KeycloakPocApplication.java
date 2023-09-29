5fc9df4baba3256e693eebe26046f1a56815be2d
akhileshmagdum <akhileshmagdum@gmail.com>
2023-09-29 12:16:40 +0530

    Project setup

---
+++ keycloak-poc/src/main/java/learn/akhilesh/keycloakpoc/KeycloakPocApplication.java
@@ -0,0 +1,13 @@
+package learn.akhilesh.keycloakpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakPocApplication.class, args);
+	}
+
+}
---
+++ keycloak-poc/src/test/java/learn/akhilesh/keycloakpoc/KeycloakPocApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.akhilesh.keycloakpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
